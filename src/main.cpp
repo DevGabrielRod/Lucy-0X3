0d3c5fb69fb1e9bedb14047981f1bea6fee662ee
Joao Gabriel Rodrigues <gabrieljdev@gmail.com>
2023-02-21 23:37:14 -0300

    Falha na Implementaçao

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,106 @@
 #include <iostream>
+#include <stdlib.h> //Para usar códigos do terminal "system()"
 
 using namespace std;
 
-extern "C" float _calc(float a, float b);
-    /*inline float som() {
-        float num1, num2, result;
-        cout << "Insira o Numero Primario:";
-        cin >> num1;
-        cout << "Insira o Numero Secundario:";
-        cin >> num2;
-        cout << "A soma de " << num1 << " e " << num2 << " e igual a: " << num1 + num2 << endl;
-        return 0.0;
-    }*/
-    inline float som() {
-        float num1, num2, result;
-        cout << "Insira o Numero Primario:";
-        cin >> num1;
-        cout << "Insira o Numero Secundario:";
-        cin >> num2;
-        cout << "A soma de " << num1 << " e " << num2 << " e igual a: " << _calc(num1, num2) << endl;
-        return 0.0;
-    }
-    inline float sub() {
-        float num1, num2, result;
-        cout << "Insira o Numero Primario:";
-        cin >> num1;
-        cout << "Insira o Numero Secundario:";
-        cin >> num2;
-        cout << "A soma de " << num1 << " e " << num2 << " e igual a: " << num1 - num2 << endl;
-        return 0.0;
-    }
-
-
+//PROTOTIPAÇÕES
+float som();
+float sub();
+float mul();
+float divi();
+void limpar(); //limpa a tela usando comando no termial de limpeza de tela
 
 int main(){
-    int opc;
-    cout << "Lucy 2.0 (Baixo Nivel)" << endl;
-    cout << "Em Que Posso Ajudar?" << endl;
-enter:
-    cout << "[ 1 ]Calculadora" << endl;
-    cin >> opc;
-
-    switch(opc) {
-        case 1:
-            som();
-            break;
-        case 2:
-            sub();
-            break;
-        default:
-            cout << "OPÇAO INVALIDA!!!" << endl
-            << "Tente Novamente!!" << endl;
-            goto enter;
-            break;
+    int opc, opccal;
+    float sair = false; //variável para definir se o loop principal deve ser quebrado
+    while(1){
+        //enter:
+        limpar();
+        cout << "Lucy 2.0 (Baixo Nivel)" << endl;
+        cout << "Em Que Posso Ajudar?" << endl;
+        cout << "[ 1 ]Calculadora" << endl;
+        cin >> opc;
+        limpar();
+        switch(opc){
+            case 1:
+                cout << "[ 1 ]Soma" << endl << "[ 2 ]Subtraçao" << endl <<"[ 3 ]Multiplicaçao" << endl <<"[ 4 ]Divisao" << endl;
+                cin >> opccal;
+                switch(opccal) {
+                    case 1:
+                        som();
+                        sair = true;
+                        break;
+                    case 2:
+                        sub();
+                        sair = true;
+                        break;
+                    case 3:
+                        mul();
+                        sair = true;
+                        break;
+                    case 4:
+                        divi();
+                        sair = true;
+                        break;
+                    default:
+                        cout << "OPÇAO INVALIDA!!!" << endl
+                        << "Tente Novamente!!" << endl;
+                        //goto enter;
+                        sair = false;
+                        break;
+                        if(sair)
+                        break;
+        }
+        
     }
-   
+    
     return 0;
 }
-
+}
+//FUNÇÕES - (DECLARAÇÕES)
+float som() {
+    float num1, num2, result;
+    cout << "Insira o Numero Primario:";
+    cin >> num1;
+    cout << "Insira o Numero Secundario:";
+    cin >> num2;
+    cout << "A soma de " << num1 << "e " << num2 << "e igual a: " << num1 + num2 << endl;
+    return 0.0;
+}
+float sub() {
+    float num1, num2, result;
+    cout << "Insira o Numero Primario:";
+    cin >> num1;
+    cout << "Insira o Numero Secundario:";
+    cin >> num2;
+    cout << "A subtraçao de " << num1 << " e " << num2 << " e igual a: " << num1 - num2 << endl;
+    return 0.0;
+}
+float mul() {
+    float num1, num2, result;
+    cout << "Insira o Numero Primario:";
+    cin >> num1;
+    cout << "Insira o Numero Secundario:";
+    cin >> num2;
+    cout << "A multiplicaçao de " << num1 << " e " << num2 << " e igual a: " << num1 * num2 << endl;
+    return 0.0;
+}
+float divi() {
+    float num1, num2, result;
+    cout << "Insira o Numero Primario:";
+    cin >> num1;
+    cout << "Insira o Numero Secundario:";
+    cin >> num2;
+    cout << "A divisao de " << num1 << " e " << num2 << " e igual a: " << num1 / num2 << endl;
+    return 0.0;
+}
+void limpar(){
+    //CÓDIGO ABAIXO PARA IDENTIFICAR O SO, CASO WINDOWS DEFINA CLS COMO O CÓDIGO CO SYSTEM
+    //CASO OUTRO DE MESMA BASE DO LINUX, DEFINA COMO CLEAR
+    #ifdef _WIN32
+        system("cls");
+    #endif
+    #ifdef __linux__
+        system("clear");
+    #endif
+}
--- src/mount.sh
+++ src/mount.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-nasmarq="main.asm"
+#nasmarq="main.asm"
 cpparg="main.cpp"
-nasmobj="main.o"
-libobj="/home/gabriel/Desktop/WorkSpace/Lucy-0X3/build/libR.o"
+#nasmobj="main.o"
+#libobj="/home/gabriel/Desktop/WorkSpace/Lucy-0X3/build/libR.o"
 
-nasm -f elf64 $nasmarq 
+#nasm -f elf64 $nasmarq 
 #nasm -f elf64 $LIBR
-g++ $nasmobj $cpparg $libobj -o execultable.ex
+g++  $cpparg -o execultable.ex
 
 
 echo "Arquivo 'execultable.ex' gerado."
 echo "Para execultar o arquivo digite './execultable.ex'."
 
-mv *.o /home/gabriel/Desktop/WorkSpace/Lucy-0X3/build
+#mv *.o /home/gabriel/Desktop/WorkSpace/Lucy-0X3/build
 cp *.ex /home/gabriel/Desktop/WorkSpace/Lucy-0X3/bin
